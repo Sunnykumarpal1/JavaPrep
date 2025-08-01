eafb9924afa5f6de06b99445f258bccded7e2d12
sunnykumarpal01 <ak2932534@gmail.com>
2025-08-01 20:09:13 +0530

    added files in jdbc

--- StreamApi/StreamMain.java
+++ StreamApi/StreamMain.java
@@ -4,16 +4,34 @@ import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        List<Integer>ls=new ArrayList<>();
+
+        List<Integer> ls = new ArrayList<>();
         ls.add(1);
         ls.add(2);
         ls.add(3);
         ls.add(4);
         ls.add(5);
         ls.add(6);
+        /*
+         * filter :- this just stores the data that is true .
+           here we have a predicated method which returns true or false 
+         */
         List<Integer> collect = ls.stream().
         filter(i->i%2==0).
         collect(Collectors.toList());
         System.out.println(collect);
+
+        /*
+         * Map: if we want to perform some opetioans on the element and store the
+         * updated one then we use this
+         */
+
+        List<Integer> collect2 =
+        ls.stream().map(i->i*2).collect(Collectors.toList());
+        System.out.println(collect2);
+
+
+        
+
     }
 }
