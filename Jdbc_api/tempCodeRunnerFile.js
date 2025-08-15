12a3d5990b16b69af8d79c944fe5bf32eccc20c9
Sunnykumarpal1 <ak2932534@gmail.com>
2025-08-15 16:45:35 +0530

    file adde

---
+++ Jdbc_api/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+
+
+
+
+function addition(a,b){
+     return a+b;
+}
+
+console.log("start");
+// opertiaon
+
+let c=5,d=10
+let ans=addition(c,d);
+console.log(ans)
+console.log("end")
---
+++ Jdbc_api/trial.js
@@ -0,0 +1,14 @@
+
+
+
+
+function addition(a,b){
+     return a+b;
+}
+
+console.log("start");
+// opertiaon
+
+
+let ans=Operations(addition);
+console.log("end")
